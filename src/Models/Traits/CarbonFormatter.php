26e7f3ce94dbde4240e65afe79b056da943aec6d
Mexion <dev@mexion.com>
2024-04-18 12:07:42 +0200

    update datetime carbon formatter

---
+++ src/Models/Traits/CarbonFormatter.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace Yormy\Dateformatter\Models\Traits;
+
+use Illuminate\Support\Carbon;
+
+trait CarbonFormatter
+{
+    public function register()
+    {
+        $dateFormat = config('dateformatter.date.format');
+        $timeFormat = config('dateformatter.time.format');
+        $dateTimeFormat = config('dateformatter.datetime.format');
+
+        Carbon::macro('formatDate', function () use ($dateFormat) {
+            return $this->format($dateFormat);
+        });
+
+        Carbon::macro('formatTime', function () use ($timeFormat) {
+            return $this->format($timeFormat);
+        });
+
+        Carbon::macro('formatDateTime', function () use ($dateTimeFormat) {
+            return $this->format($dateTimeFormat);
+        });
+    }
+}
